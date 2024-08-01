3b9e7dd6e14086a222cbb2961a5eb82c0d65ce25
aparnamagadum <aparnamagadum2@gmail.com>
2024-08-01 12:47:16 +0530

    added files

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Contact.css'
+
+function Contact(){
+    return(
+        <>
+        <div className="contact">
+           <h1>Contact Me</h1>
+           <p>Created by : Aparna Magadum</p> 
+           <p>Email : aparnamagadum2@gmail.com</p>
+           <p>Phone : 7338193202</p>
+        </div>
+        </>
+    )
+}
+
+export default Contact;
